358c0d226c49c5776f39b8f572f962a6a6d4d945
marcelogf20 <mguedesfranca@gmail.com>
2019-03-27 12:03:27 -0300

    .

--- Modelos/FC_modelo.py
+++ Modelos/FC_modelo.py
@@ -10,19 +10,16 @@ class EncoderFC(nn.Module):
     """
     def __init__(self, coded_size, patch_size):
         super(EncoderFC, self).__init__()
-        print('here1')
         self.patch_size = patch_size
         self.coded_size = coded_size
 
-        self.fc1 = nn.Linear(3 * patch_size * patch_size, 512)
-        self.fc2 = nn.Linear(512, 512)
-        self.fc3 = nn.Linear(512, 512)
-        self.w_bin = nn.Linear(512, self.coded_size)
+        self.fc1 = nn.Linear(3 * patch_size * patch_size, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 10)
+        self.w_bin = nn.Linear(10, self.coded_size)
         self.binary = BinaryLayer()
     
-    def forward(self, x):
-        print('here2')
-    
+    def forward(self, x):   
         """
         :param x: image typically a 8x8@3 patch image
         :return:
@@ -46,10 +43,10 @@ class DecoderFC(nn.Module):
         self.patch_size = patch_size
         self.coded_size = coded_size
 
-        self.fc1 = nn.Linear(coded_size, 512)
-        self.fc2 = nn.Linear(512, 512)
-        self.fc3 = nn.Linear(512, 512)
-        self.last_layer = nn.Linear(512, patch_size * patch_size * 3)
+        self.fc1 = nn.Linear(coded_size, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 10)
+        self.last_layer = nn.Linear(10, patch_size * patch_size * 3)
 
     def forward(self, x):
         """
--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import os
 import numpy as np
 from Modelos import*
 
-def importar_dados():
+def main(model,coded_size, patch_size,num_epochs,batch_size,lr_rate,w_d,log_step,model_path,save_step):
 	   # Create the model directory if does not exist
-    if not os.path.exists(args.model_path):
-        os.makedirs(args.model_path)
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
 
     # Normalize function so given an image of range [0, 1] transforms it into a Tensor range [-1. 1]
     transform = transforms.Compose([
@@ -24,9 +24,8 @@ def importar_dados():
 
     # Load the CIFAR LOADER
     trainset = torchvision.datasets.CIFAR100(root='./data', train=True, download=False, transform=transform)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
-def model_training(coded_size, patch_size):
     modeloFC= FC_modelo.CoreFC(coded_size, patch_size)
     print(modeloFC)
    
@@ -34,13 +33,99 @@ def model_training(coded_size, patch_size):
     criterion = nn.MSELoss()
     params = list(modeloFC.parameters())
     print(len(params))
-    optimizer = optim.Adam(params, lr=0.01, weight_decay=0)
+    optimizer = optim.Adam(params, lr=lr_rate, weight_decay=w_d)
 
     # ::::::::::::::::::::::::::::::::
     #   TRAIN----------------------
     # ::::::::::::::::::::::::::::::::
 
+    num_steps = len(train_loader)
+    start = time.time()
+    total_losses = []
+    # Divide the input 32x32 images into num_patches patch_sizexpatch_size patchs
+    num_patches = (32//patch_size)**2
+    print((num_patches))
+
+    for epoch in range(num_epochs):
+
+        running_loss = 0.0
+        current_losses = []
+        for i, data in enumerate(train_loader, 0):
+
+            # Get the images
+            imgs = data[0]
+
+            # Transform into patches
+            patches = to_patches(imgs, patch_size)
+            if i==0:
+                print(patches[0])
+            # TODO: Do this thing more polite!! :S
+            for patch in patches:
+                # Transform the tensor into Variable
+                v_patch = Variable(patch)
+                target_tensor = Variable(torch.zeros(v_patch.size()), requires_grad=False)
+                losses = []
+                    # Set gradients to Zero
+                optimizer.zero_grad()
+                reconstructed_patches = modeloFC(v_patch)
+                loss = criterion(reconstructed_patches, v_patch)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+
+            
+
+            # STATISTICS:
+
+            if (i+1) % log_step == 0:
+                print('(%s) [%d, %5d] loss: %.3f' %
+                      (timeSince(start, ((epoch * num_steps + i + 1.0) / (num_epochs * num_steps))),
+                       epoch + 1, i + 1, running_loss / log_step / num_patches))
+                current_losses.append(running_loss/log_step/num_patches)
+                running_loss = 0.0
+
+            # SAVE:
+            if (i + 1) % save_step == 0:
+                torch.save(modeloFC.state_dict(),
+                           os.path.join(model_path, model+'-p%d_b%d-%d_%d.pkl' %
+                                        (patch_size, coded_size, epoch + 1, i + 1)))
+
+        total_losses.append(current_losses)
+        torch.save(modeloFC.state_dict(),
+                   os.path.join(model_path,
+                                model + '-p%d_b%d-%d_%d.pkl' % (patch_size, coded_size, epoch + 1, i + 1)))
+
+    print('__TRAINING DONE=================================================')
 
 
-modelo=model_training(4,8)
+#==============================================
+# - CUSTOM FUNCTIONS
+#==============================================
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / percent
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+
+
+def to_patches(x, patch_size):
+    num_patches_x = 32//patch_size
+    patches = []
+    for i in range(num_patches_x):
+        for j in range(num_patches_x):
+            patch = x[:, :, i*patch_size:(i+1)*patch_size, j*patch_size:(j+1)*patch_size]
+            patches.append(patch.contiguous())
+    return patches
+
+
+if __name__ == '__main__':
+    main('fc',4, 8,1,4,0.01,0,10,'./saved_models/',50)
 	
